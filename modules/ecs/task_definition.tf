c727c979d469680282d3bd1a62606183b0245903
Feng Frank <emir_feng@163.com>
2025-09-20 16:18:54 +0800

    grafana dashboard

--- modules/ecs/task_definition.tf
+++ modules/ecs/task_definition.tf
@@ -4,8 +4,8 @@ resource "aws_ecs_task_definition" "prometheus_yace" {
   family                   = "prometheus-yace"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "512"
-  memory                   = "1024"
+  cpu                      = "2048"
+  memory                   = "4096"
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
@@ -16,10 +16,86 @@ resource "aws_ecs_task_definition" "prometheus_yace" {
       portMappings = [{
         containerPort = 9090
       }]
-      mountPoints = [{
-        sourceVolume  = "prometheus-config"
-        containerPath = "/etc/prometheus"
-      }]
+      mountPoints = [
+        {
+          sourceVolume  = "prometheus-config"
+          containerPath = "/etc/prometheus"
+        },
+        {
+          sourceVolume  = "prometheus-data"
+          containerPath = "/prometheus"
+        }
+      ]
+      command = [
+        "--config.file=/etc/prometheus/prometheus.yml",
+        "--storage.tsdb.path=/prometheus",
+        "--web.console.libraries=/etc/prometheus/console_libraries",
+        "--web.console.templates=/etc/prometheus/consoles",
+        "--storage.tsdb.retention.time=2h",
+        "--storage.tsdb.retention.size=1GB",
+        "--storage.tsdb.wal-compression",
+        "--web.enable-lifecycle",
+        "--web.enable-admin-api",
+        "--web.external-url=http://localhost:9090/",
+        "--web.route-prefix=/"
+      ]
+    },
+    {
+      name      = "thanos-sidecar"
+      image     = "quay.io/thanos/thanos:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 10901
+        },
+        {
+          containerPort = 10902
+        }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "prometheus-data"
+          containerPath = "/prometheus"
+        }
+      ]
+      command = [
+        "sidecar",
+        "--tsdb.path=/prometheus",
+        "--prometheus.url=http://localhost:9090",
+        "--http-address=0.0.0.0:10902",
+        "--grpc-address=0.0.0.0:10901",
+        "--s3.config-file=/etc/thanos/s3-config.yaml"
+      ]
+      environment = [
+        {
+          name  = "AWS_REGION"
+          value = "us-east-1"
+        }
+      ]
+    },
+    {
+      name      = "thanos-query"
+      image     = "quay.io/thanos/thanos:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 10903
+        },
+        {
+          containerPort = 10904
+        }
+      ]
+      command = [
+        "query",
+        "--http-address=0.0.0.0:10903",
+        "--grpc-address=0.0.0.0:10904",
+        "--query.replica-label=replica",
+        "--store=thanos-sidecar:10901",
+        "--query.auto-downsampling",
+        "--query.partial-response",
+        "--query.max-concurrent=20",
+        "--query.timeout=30s"
+      ]
     },
     {
       name      = "yace"
@@ -32,6 +108,12 @@ resource "aws_ecs_task_definition" "prometheus_yace" {
         sourceVolume  = "yace-config"
         containerPath = "/config"
       }]
+      environment = [
+        {
+          name  = "AWS_REGION"
+          value = "us-east-1"
+        }
+      ]
     }
   ])
 
@@ -40,6 +122,15 @@ resource "aws_ecs_task_definition" "prometheus_yace" {
     efs_volume_configuration {
       file_system_id = var.efs_id
       root_directory = "/prometheus"
+      transit_encryption = "ENABLED"
+    }
+  }
+
+  volume {
+    name = "prometheus-data"
+    efs_volume_configuration {
+      file_system_id = var.efs_id
+      root_directory = "/prometheus-data"
       transit_encryption = "ENABLED"
     }
   }
